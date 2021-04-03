cf288a734b79c136b1975c8231ba2f7b1496ef6d
Paralelopipet <damjan.denic@gmail.com>
2021-04-04 01:29:01 +0200

    aqi calculator

---
+++ useless_folder/aqi.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def calculate_aqi(co, no2, o3, pm10, p25, so2):
+    
+    boundaries = {
+    "pm10" : [0, 50, 100, 250, 350, 430],
+    "p25" : [0, 30, 60, 90, 120, 250],
+    "no2" : [0, 40, 80, 180, 280, 400],
+    "o3" : [0, 50, 100, 168, 208, 748],
+    "co" : [0, 1, 2, 10, 17, 34],
+    "so2" : [0, 40, 80, 380, 800, 1600],
+    "aqi" : [0, 50, 100, 200, 300, 400, 500]
+    }
+    data = {
+        "co" : co,
+        "no2" : no2,
+        "o3" : o3,
+        "pm10" : pm10,
+        "p25" : p25,
+        "so2" : so2
+    }
+    pollusions = ["co", "no2", "o3", "pm10", "p25", "so2"]
+
+    aqi_max = 0
+    for pol in pollusions:
+        j = len(boundaries[pol])-1
+        while data[pol] < boundaries[pol][j]:
+            j-=1
+        low_index = j
+        high_index = j+1
+        aqi = ((boundaries["aqi"][high_index] - boundaries["aqi"][low_index]) / \
+                 (boundaries[pol][high_index] - boundaries[pol][low_index])) * \
+                (data[pol] - boundaries[pol][low_index]) + boundaries["aqi"][low_index]
+        if aqi > aqi_max:
+            aqi_max = aqi
+    return aqi_max
+
+
